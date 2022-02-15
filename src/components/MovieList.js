52df57c0bb37a866da66ad1c658c539e03dbaee6
Diego Valero <diego27jay@gmail.com>
2022-02-14 17:33:02 -0700

    react router work

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,3 +1,12 @@
 import React from "react";
 
 // this is where we map through the json to get every movie
+function MovieList() {
+    return (
+      <div>
+        <h1>All Movies</h1>
+      </div>
+    );
+  }
+
+export default MovieList
--- src/components/TopMovieList.js
+++ src/components/TopMovieList.js
@@ -1,2 +1,23 @@
 // this is where users add their own list using a form
 import React from "react";
+
+
+function TopMovieList() {
+    return (
+      <div>
+        <h1>Add Movie</h1>
+        <form>
+          <div>
+            <input type="text" name="movie name" placeholder="Movie Name" />
+          </div>
+          <div>
+            <input type="image" name="image" placeholder="Image" />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+
+export default TopMovieList
+
